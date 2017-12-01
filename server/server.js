1272a61e85c8ec76a51dc6d67287a11f65edcf9d
renliangbo <renna0508@126.com>
2017-12-01 09:33:34 +0800

    restAPI

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var mongoose = require('./db/mongoose')
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
 
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+		let todo = new Todo({text: req.body.text});
+
+		todo
+				.save()
+				.then((todo) => {
+						res.send(todo)
+				}, (err) => {
+						res
+								.status(400)
+								.send(err)
+				})
+})
 app.listen(3000, () => {
 		console.log('Started on port 3000')
 });
