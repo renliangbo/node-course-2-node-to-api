42b567e7be80f6c9a2025aba78c54127b78ee47f
renliangbo <renna0508@126.com>
2017-12-03 21:43:09 +0800

    add auth middleware

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ require('../config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
-const _ = require('lodash')
+const _ = require('lodash');
+const {authenticate} = require('./middleware/authenticate')
 
 var mongoose = require('./db/mongoose')
 var {Todo} = require('./models/todo');
@@ -141,14 +142,8 @@ app.post('/users', (req, res) => {
 				})
 });
 
-app.get('/users/me', (req, res) => {
-		var token = req.header('x-auth');
-		User
-				.findByToken(token)
-				.then((user) => {
-						if (!user) {};
-						res.send(user);
-				});
+app.get('/users/me', authenticate, (req, res) => {
+		res.send(req.user);
 })
 app.listen(port, () => {
 		console.log(`Started on port${port}`)
