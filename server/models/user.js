63ffebf18bb6a4651e04205f7781538c39f94e1b
renliangbo <renna0508@126.com>
2017-12-03 16:01:58 +0800

    post /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const {mongoose} = require('../db/mongoose');
-
+const validator = require('validator');
 const {Schema} = mongoose;
 
 const UserSchema = new Schema({
@@ -7,8 +7,31 @@ const UserSchema = new Schema({
 				type: String,
 				required: true,
 				trim: true,
-				minlength: 1
-		}
+				minlength: 1,
+				unique: true,
+				validate: {
+						validator: value => validator.isEmail(value),
+						message: '{VALUE} is not a valid email'
+				}
+		},
+		password: {
+				type: String,
+				required: true,
+				minlength: 6
+		},
+		tokens: [
+				{
+						access: {
+								type: String,
+								required: true
+						},
+						token: {
+								type: String,
+								required: true
+						}
+				}
+		]
+
 });
 
 const User = mongoose.model('User', UserSchema);
--- server/server.js
+++ server/server.js
@@ -121,6 +121,20 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
+		var body = _.pick(req.body, ['email', 'password']);
+		var user = User(body);
+		user
+				.save()
+				.then((user) => {
+						res.send(user)
+				})
+				.catch((err) => {
+						res
+								.status(400)
+								.send(err)
+				})
+})
 app.listen(port, () => {
 		console.log(`Started on port${port}`)
 });
