cdd9b4e63b0e7978ab6953f63532b74461ea2501
renliangbo <renna0508@126.com>
2017-12-04 15:54:52 +0800

    add POST/user/login

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,27 @@ UserSchema.statics.findByToken = function (token) {
 		};
 		return User.findOne({'_id': decoded._id, 'tokens.token': token, 'tokens.access': 'auth'})
 };
+
+UserSchema.statics.findByCredentials = function (email, password) {
+		var User = this;
+		return User
+				.findOne({email})
+				.then((user) => {
+						if (!user) {
+								return Promise.reject();
+						};
+						return new Promise((resolve, reject) => {
+								bcrypt.compare(password, user.password, (err, res) => {
+										if (res) {
+												resolve(user);
+										} else {
+												reject(user)
+										}
+								})
+						})
+				})
+}
+
 UserSchema.pre('save', function (next) {
 		var user = this;
 		if (user.isModified('password')) {
